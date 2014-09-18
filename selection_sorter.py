a20f65eb4e247591108ca0b6331a40f0eb67b293
Sascha Wolf <swolf.dev@gmail.com>
2014-09-18 14:31:18 +0200

    Plugin: Fix selection after sorting

--- selection_sorter.py
+++ selection_sorter.py
@@ -21,10 +21,13 @@ class SortSelection(sublime_plugin.TextCommand):
             non_duplicate_selections.append(selection)
             non_duplicate_lines.append(self.view.line(selection))
 
-
-
         sorted_regions = self._sort_regions(non_duplicate_selections)
-        sorted_lines_text = [self.view.substr(self.view.line(r)) for r in sorted_regions]
+        sorted_lines = []
+        sorted_lines_text = []
+        for region in sorted_regions:
+            line = self.view.line(region)
+            sorted_lines.append(line)
+            sorted_lines_text.append(self.view.substr(line))
 
         selections.clear()
         for i in reversed(range(len(non_duplicate_selections))):
@@ -35,14 +38,14 @@ class SortSelection(sublime_plugin.TextCommand):
                               sorted_lines_text[i])
 
             # Update the selection
-            start = non_duplicate_selections[i].begin()
+            start = non_duplicate_lines[i].begin() + (sorted_regions[i].begin() - sorted_lines[i].begin())
             end = start + sorted_regions[i].size()
             selections.add(sublime.Region(start, end))
 
-    def _sort_regions(self, selection):
+    def _sort_regions(self, selections):
         morph = lambda s: s
         if self.case_insensitive:
             morph = lambda s: s.lower()
 
         # Sort the selection regions based on the text
-        return sorted(selection, key=lambda r: morph(self.view.substr(r)))
+        return sorted(selections, key=lambda r: morph(self.view.substr(r)))
